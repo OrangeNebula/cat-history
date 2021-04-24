c0e15a5fc834cd7cbbfb6cbde6d107964179aa96
rednebula <rednebula01018@gmail.com>
2021-04-24 21:57:25 +0900

    [etc] Add test code for cat service

---
+++ src/cat/cat.service.spec.ts
@@ -0,0 +1,39 @@
+import { CatService } from './cat.service';
+import { Test } from '@nestjs/testing';
+import { catProviders } from './cat.providers';
+import { CatClientMapper } from './mapper/cat-client.mapper';
+import { DatabaseModule } from '../database/database.module';
+import { Cat } from './cat.entity';
+
+describe('Cat service test', () => {
+  let catService: CatService;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [DatabaseModule],
+      providers: [
+        ...catProviders,
+        CatService,
+        CatClientMapper,
+      ]
+    }).compile();
+
+    catService = module.get<CatService>(CatService);
+  })
+
+  describe('create', () => {
+    let cat: Cat;
+    it('should create new cat item', async () => {
+      cat = await catService.create({
+        name: '테스트 냥',
+        description: '난 테스트 냥이다.',
+      });
+      expect(cat.name).toBe('테스트 냥');
+    });
+
+    it('should return created cat item', async () => {
+      const result = await catService.find(cat.id);
+      expect(result.name).toBe('테스트 냥');
+    });
+  });
+});
--- src/database/seed/cat.seeder.ts
+++ src/database/seed/cat.seeder.ts
@@ -2,13 +2,15 @@ import { Seeder } from './seeder';
 import { Connection } from 'typeorm';
 import { Cat } from '../../cat/cat.entity';
 
+export const catSeedData = [
+  new Cat('젖소냥', '새로 만난 젖소냥이!'),
+  new Cat('염소냥', '새로 만난 염소냥이!'),
+  new Cat('바둑냥', '새로 만난 바둑냥이!'),
+]
+
 export class CatSeeder implements Seeder {
   async run(connection: Connection): Promise<any> {
     const repository = connection.getRepository(Cat);
-    await Promise.all([
-      repository.save(new Cat('젖소냥', '새로 만난 젖소냥이!')),
-      repository.save(new Cat('염소냥', '새로 만난 염소냥이!')),
-      repository.save(new Cat('바둑냥', '새로 만난 바둑냥이!')),
-    ]);
+    await Promise.all(catSeedData.map(item => repository.save(item)))
   }
 }
